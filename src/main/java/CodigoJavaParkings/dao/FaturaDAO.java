e7355d95e0d030a592b0e2e933125566826ff0c6
Thomas Ramos Oliveira <161534687+Thomasramos02@users.noreply.github.com>
2024-11-06 17:36:16 -0300

    Update FaturaDAO.java

--- src/main/java/CodigoJavaParkings/dao/FaturaDAO.java
+++ src/main/java/CodigoJavaParkings/dao/FaturaDAO.java
@@ -1,9 +1,13 @@
 package dao;
 
 import model.Fatura;
+import java.util.Date;
 import java.util.List;
 
 public interface FaturaDAO {
-    void salvar(Fatura fatura);       // Salva uma nova fatura
-    List<Fatura> buscarTodas();       // Retorna todas as faturas salvas
+    void salvar(Fatura fatura);             // Salva uma nova fatura
+    List<Fatura> buscarTodas();             // Retorna todas as faturas salvas
+
+    // Novo método para buscar faturas filtradas por intervalo de datas e placa
+    List<Fatura> buscarPorDataEPlaca(Date dataInicio, Date dataFim, String placa);
 }
